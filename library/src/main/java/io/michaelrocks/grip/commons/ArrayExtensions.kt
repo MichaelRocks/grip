007b4681fe978f04301cfbff18ef82f575362401
Michael Rozumyanskiy <michael.rozumyanskiy@gmail.com>
2019-02-15 09:17:03 +0300

    Move lambdas out of parentheses

--- library/src/main/java/io/michaelrocks/grip/commons/ArrayExtensions.kt
+++ library/src/main/java/io/michaelrocks/grip/commons/ArrayExtensions.kt
@@ -17,9 +17,9 @@
 package io.michaelrocks.grip.commons
 
 internal inline fun <T, reified R> Array<out T>.mapToArray(transform: (T) -> R): Array<R> {
-  return Array(size, { index -> transform(this[index]) })
+  return Array(size) { index -> transform(this[index]) }
 }
 
 internal inline fun <T, reified R> List<T>.mapToArray(transform: (T) -> R): Array<R> {
-  return Array(size, { index -> transform(this[index]) })
+  return Array(size) { index -> transform(this[index]) }
 }
